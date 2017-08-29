76338888269eed834cc78ecf3b52c706459e8d2e
Ilker Ozcan <ilker.ozcan@veripark.com>
2017-08-29 21:29:26 +0300

    Added common constants.

---
+++ Common/Constants/IOCommonConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IOBootstrap.NET.Common.Constants
+{
+    public static class IOCommonConstants
+    {
+
+        #region Properties
+
+        public static string DefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss";
+
+        #endregion
+
+    }
+}

623f3552f47a8a9d537ab68b0de7b6f2f716d568
Ilker Ozcan <ilker.ozcan@veripark.com>
2017-08-30 18:32:13 +0300

    Added password hash helper.

--- Common/Utilities/IOCommonHelpers.cs
+++ Common/Utilities/IOCommonHelpers.cs
@@ -2,11 +2,20 @@
 using System.Linq;
 using System.Reflection;
 using System;
+using System.Security.Cryptography;
 
 namespace IOBootstrap.NET.Common.Utilities
 {
     public static class IOCommonHelpers
     {
+
+		#region Constants
+
+		private const int HashSize = 64;
+        private const int SaltSize = 32;
+
+        #endregion
+
         #region Application Version
 
         public static string CurrentVersion
@@ -40,13 +49,85 @@ namespace IOBootstrap.NET.Common.Utilities
 
         #region Random Helpers
 
-        public static string GenerateRandomAlphaNumericString(int characterCount) {
-			const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
-			return new string(Enumerable.Repeat(chars, characterCount)
+        public static string GenerateRandomAlphaNumericString(int characterCount)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, characterCount)
                               .Select(s => s[new Random().Next(s.Length)]).ToArray());
         }
 
-#endregion
+        #endregion
+
+        #region Hash
+
+        public static string Hash(string password, int iterations)
+		{
+			//create salt
+			byte[] salt;
+			new RNGCryptoServiceProvider().GetBytes(salt = new byte[SaltSize]);
+
+			//create hash
+			var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+			var hash = pbkdf2.GetBytes(HashSize);
+
+			//combine salt and hash
+			var hashBytes = new byte[SaltSize + HashSize];
+			Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+			Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+
+			//convert to base64
+			var base64Hash = Convert.ToBase64String(hashBytes);
+
+			//format hash with extra information
+			return string.Format("$IOPSSWD$V1${0}${1}", iterations, base64Hash);
+		}
+
+		public static string Hash(string password)
+		{
+			return Hash(password, 10000);
+		}
+
+		public static bool IsHashSupported(string hashString)
+		{
+			return hashString.Contains("$IOPSSWD$V1$");
+		}
+
+		public static bool Verify(string password, string hashedPassword)
+		{
+			//check hash
+			if (!IsHashSupported(hashedPassword))
+			{
+				throw new NotSupportedException("The hashtype is not supported");
+			}
+
+			//extract iteration and Base64 string
+			var splittedHashString = hashedPassword.Replace("$IOPSSWD$V1$", "").Split('$');
+			var iterations = int.Parse(splittedHashString[0]);
+			var base64Hash = splittedHashString[1];
+
+			//get hashbytes
+			var hashBytes = Convert.FromBase64String(base64Hash);
+
+			//get salt
+			var salt = new byte[SaltSize];
+			Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+
+			//create hash with given salt
+			var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+			byte[] hash = pbkdf2.GetBytes(HashSize);
+
+			//get result
+			for (var i = 0; i < HashSize; i++)
+			{
+				if (hashBytes[i + SaltSize] != hash[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+        #endregion
 
     }
 }

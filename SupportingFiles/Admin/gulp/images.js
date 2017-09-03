c86e5a506b0c28005134c8170e1ca5718bf66703
Ilker Ozcan <ilker.ozcan@veripark.com>
2017-09-03 23:12:13 +0300

    Added admin page supporting files.

---
+++ SupportingFiles/Admin/gulp/images.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+gulp.task('copyVendorImages', function () {
+  return gulp
+    .src([
+      path.join(conf.wiredep.directory, '**/ionrangeslider/img/**/*'),
+      path.join(conf.wiredep.directory, '**/jstree/dist/themes/**/*'),
+      path.join(conf.wiredep.directory, '**/leaflet/dist/images/**/*')
+    ])
+    .pipe(gulp.dest(path.join(conf.paths.tmp, 'serve', '/assets/img/theme/vendor')));
+});
+

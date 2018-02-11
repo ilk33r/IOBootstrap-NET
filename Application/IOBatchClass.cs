b22c59c60680c4c32b665a11264db290b66b9c8d
Ilker Ozcan <ilker.ozcan@veripark.com>
2018-02-12 00:10:57 +0300

    Added batch startup classes.

---
+++ Application/IOBatchClass.cs
@@ -0,0 +1,44 @@
+﻿using IOBootstrap.NET.Core.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace IOBootstrap.NET.Application
+{
+    public abstract class IOBatchClass<TDBContext> where TDBContext : IODatabaseContext<TDBContext>
+    {
+
+        #region Properties
+
+        public bool _isDevelopment { get; set; }
+        public IConfiguration _configuration { get; set; }
+        public String _configurationPath { get; set; }
+		public TDBContext _databaseContext { get; set; }
+        public ILogger _logger { get; set; }
+
+        #endregion
+
+        #region Initialization Methods
+
+        public IOBatchClass(bool isDevelopment, 
+                            IConfiguration configuration,
+                            string configurationPath, 
+                            TDBContext databaseContext, 
+                            ILogger logger)
+        {
+            // Setup properties
+            this._isDevelopment = isDevelopment;
+            this._configuration = configuration;
+            this._configurationPath = configurationPath;
+            this._databaseContext = databaseContext;
+            this._logger = logger;
+        }
+
+        public virtual void Run()
+        {
+        }
+
+        #endregion
+
+    }
+}
---
+++ Application/IOBatchStartup.cs
@@ -0,0 +1,125 @@
+﻿using IOBootstrap.NET.Core.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace IOBootstrap.NET.Application
+{
+    public abstract class IOBatchStartup<TDBContext> where TDBContext : IODatabaseContext<TDBContext>
+    {
+
+        #region Properties
+
+        protected bool IsDevelopment { get; set; }
+        protected IConfiguration Configuration { get; set; }
+        protected string ConfigurationPath { get; set; }
+        protected TDBContext DatabaseContext { get; set; }
+        protected ILogger Logger { get; set; }
+
+        #endregion
+
+        #region Initialization Methods
+
+        public IOBatchStartup(string configFilePath, bool development)
+        {
+            // Setup properties
+            this.IsDevelopment = development;
+            this.Configuration = this.GetConfigurations(configFilePath);
+            this.ConfigurationPath = configFilePath;
+			this.Logger = new ConsoleLogger(this.BatchName(), null, true);
+
+            // Create database context
+            DbContextOptionsBuilder databaseOptionBuilder = new DbContextOptionsBuilder();
+            Type databaseContextType = this.GetType().BaseType.GenericTypeArguments[0];
+            DbContextOptionsBuilder<TDBContext> databaseOptionsBuilder = new DbContextOptionsBuilder<TDBContext>();
+            this.DatabaseContextOptions(databaseOptionsBuilder);
+            object[] parameters = new object[] {
+                (object)databaseOptionsBuilder.Options
+            };
+            this.DatabaseContext = (TDBContext)Activator.CreateInstance(this.GetType().BaseType.GenericTypeArguments[0], parameters);
+
+        }
+
+        public void RunBatch(Type batchClass)
+        {
+            // Create batch class instance
+            object[] parameters = new object[] {
+                (object)this.IsDevelopment,
+                (object)this.Configuration,
+                (object)this.ConfigurationPath,
+                (object)this.DatabaseContext,
+                (object)this.Logger
+            };
+
+            IOBatchClass<TDBContext> batchInstance = (IOBatchClass<TDBContext>)Activator.CreateInstance(batchClass, parameters);
+            batchInstance.Run();
+        }
+
+        public virtual void RunAllBatches()
+        {
+            // Obtain batch classes
+            Type[] batchClasses = this.BatchClasses();
+
+            // Loop Batch Classes
+            foreach (Type batchClass in batchClasses)
+            {
+                this.RunBatch(batchClass);
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private IConfiguration GetConfigurations(string configFilePath) 
+        {
+            // Create environment name
+            string environmentName;
+
+            // Check environment is development
+            if (IsDevelopment)
+            {
+                environmentName = "Development";
+            }
+            else
+            {
+                environmentName = "Production";
+            }
+
+            // Create builder
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(configFilePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            // Setup properties
+            return builder.Build();
+        }
+
+        #endregion
+
+        #region Configs
+
+        public virtual string BatchName()
+        {
+            return "IOBatch";
+        }
+
+        public virtual Type[] BatchClasses()
+        {
+            return new Type[] {};
+        }
+
+        public virtual void DatabaseContextOptions(DbContextOptionsBuilder<TDBContext> options)
+        {
+            options.UseInMemoryDatabase("IOMemory");
+        }
+
+        #endregion
+    }
+}

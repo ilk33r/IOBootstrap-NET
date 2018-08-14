0f7d795344528b66f5942cdf995215133b784544
Ilker Ozcan <ilker.ozcan@veripark.com>
2018-08-15 00:06:56 +0300

    Added culture and version to request model.

---
+++ Common/Enumerations/CultureTypes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace IOBootstrap.NET.Common.Enumerations
+{
+    public enum CultureTypes
+    {
+        En = 0,
+        Tr = 1
+    }
+}
--- Common/Models/BaseModels/IORequestModel.cs
+++ Common/Models/BaseModels/IORequestModel.cs
@@ -1,4 +1,5 @@
 ﻿using IOBootstrap.NET.WebApi.BackOffice.Models;
+﻿using IOBootstrap.NET.Common.Enumerations;
 using System;
 
 namespace IOBootstrap.NET.Common.Models.BaseModels
@@ -9,6 +10,8 @@ namespace IOBootstrap.NET.Common.Models.BaseModels
         #region Properties
 
         public IOClientInfoModel ClientInfo { get; set; }
+        public CultureTypes Culture;
+        public string Version;
 
         #endregion
     }

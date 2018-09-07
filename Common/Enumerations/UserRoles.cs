5518418504b453b0b3c9b7c0bd704fc49bef6151
Ilker Ozcan <ilker.ozcan@veripark.com>
2018-09-07 18:56:03 +0300

    Added user roles enumeration.

---
+++ Common/Enumerations/UserRoles.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace IOBootstrap.NET.Common.Enumerations
+{
+    public enum UserRoles
+    {
+        SuperAdmin = 0,
+        Admin = 1,
+        User = 2
+    }
+
+    public static class UserRoleUtility 
+    {
+        public static bool CheckRole(UserRoles requiredRole, UserRoles userRole) 
+        {
+            int userRoleValue = (int)userRole;
+            int requiredRoleValue = (int)requiredRole;
+
+            return userRole <= requiredRole;
+        }
+    }
+}

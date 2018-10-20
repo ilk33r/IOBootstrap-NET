474817959726371e8e679a9330f4312cfee63b44
Ilker Ozcan <ilker.ozcan@veripark.com>
2018-10-21 01:37:31 +0300

    Added sample batch classes.

---
+++ SupportingFiles/Batch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BoomAppBatch.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check argument count is correct
+            if (args.Length != 2) {
+                Console.WriteLine("Incorrect parameters");
+                Console.WriteLine("Usage: [env] [config path]");
+                return;
+            }
+
+            // Obtain is arguments
+            bool development = (args[0].Equals("dev")) ? true : false;
+            string configPath = args[1];
+
+            // Start batch
+            IOBatchStartup startup = new IOBatchStartup(configPath, development);
+            startup.RunAllBatches();
+        }
+    }
+}

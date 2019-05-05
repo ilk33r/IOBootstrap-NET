4128e96d9c9673faeaa4779585e3e3044a349ad2
Ilker Ozcan <ilker.ozcan@veripark.com>
2019-05-05 21:43:21 +0300

    Added cache utilities.

---
+++ Core/Cache/IOCache.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IOBootstrap.NET.Core.Cache.Utilities
+{
+    public static class IOCache
+    {
+
+        #region Privates
+
+        private static List<IOCacheObject> CachedObjects;
+
+        #endregion
+
+        #region Public Methods
+
+        public static void CacheObject(IOCacheObject cache)
+        {
+            IOCache.InitializeCache();
+
+            if (IOCache.CacheExists(cache) < 0)
+            {
+                cache.SetCacheTime();
+                IOCache.CachedObjects.Add(cache);
+            }
+        }
+
+        public static IOCacheObject GetCachedObject(string key)
+        {
+            IOCache.InitializeCache();
+            IOCacheObject cachedObject = IOCache.CachedObjects.Find((obj) => obj.GetKey().Equals(key));
+            return cachedObject;
+        }
+
+        public static void InvalidateCache(string key)
+        {
+            int index = IOCache.CachedObjects.FindIndex((obj) => obj.GetKey().Equals(key));
+            if (index >= 0)
+            {
+                IOCache.CachedObjects.RemoveAt(index);
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private static int CacheExists(IOCacheObject cache)
+        {
+            int index = IOCache.CachedObjects.FindIndex((obj) => obj.GetCacheID() == cache.GetCacheID());
+            return index;
+        }
+
+        private static void InitializeCache()
+        {
+            if (IOCache.CachedObjects == null)
+            {
+                IOCache.CachedObjects = new List<IOCacheObject>();
+            }
+
+            DateTimeOffset currentTimeOffset = DateTimeOffset.Now;
+            long currentTimeStamp = currentTimeOffset.ToUnixTimeSeconds();
+            List<int> forRemoveIndexes = new List<int>();
+
+            foreach (IOCacheObject cache in IOCache.CachedObjects)
+            {
+                if (cache.GetCacheEndTimeStamp() < currentTimeStamp)
+                {
+                    int index = IOCache.CacheExists(cache);
+                    if (index >= 0)
+                    {
+                        forRemoveIndexes.Add(index);
+                    }
+                }
+            }
+
+            foreach (int index in forRemoveIndexes)
+            {
+                IOCache.CachedObjects.RemoveAt(index);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Core/Cache/IOCacheObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using IOBootstrap.NET.Common.Utilities;
+
+namespace IOBootstrap.NET.Core.Cache
+{
+    public class IOCacheObject
+    {
+
+        #region Privates
+
+        private long CacheInterval;
+        private long CacheTime;
+        private string CacheID;
+        private string Key;
+
+        #endregion
+
+        #region Publics
+
+        public Object Value;
+
+        #endregion
+
+        #region Initialization Methods
+
+        public IOCacheObject(string key, Object objectValue, long cacheInterval)
+        {
+            this.CacheInterval = cacheInterval;
+            this.Key = key;
+            this.Value = objectValue;
+            this.CacheID = IORandomUtilities.GenerateGUIDString();
+        }
+
+        #endregion
+
+        #region Getters
+
+        public string GetCacheID()
+        {
+            return this.CacheID;
+        }
+
+        public long GetCacheEndTimeStamp()
+        {
+            return this.CacheTime + this.CacheInterval;
+        }
+
+        public string GetKey()
+        {
+            return this.Key;
+        }
+
+        #endregion
+
+        #region Setters
+
+        public void SetCacheTime()
+        {
+            DateTimeOffset timeOffset = DateTimeOffset.Now;
+            this.CacheTime = timeOffset.ToUnixTimeSeconds();
+        }
+
+        #endregion
+    }
+}

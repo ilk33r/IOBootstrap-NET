a643530e70baeec890d22a3f744f90bf105ef889
Ilker OZCAN <ilker.ozcan@veripark.com>
2020-07-25 17:51:21 +0300

    HTTP client was migrated with netcore3.1

--- Core/HTTP/IOHTTPClient.cs
+++ Core/HTTP/IOHTTPClient.cs
@@ -3,7 +3,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using IOBootstrap.NET.Common.Models.Shared;
+using IOBootstrap.NET.Common.Models.Base;
 using IOBootstrap.NET.Core.HTTP.Enumerations;
 using Newtonsoft.Json;
 

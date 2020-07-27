7cb7fb094004ec57a8db92402c0375c8370d3284
Ilker OZCAN <ilker.ozcan@veripark.com>
2020-07-27 14:36:23 +0300

    Default configuration controller name was changed.

--- BackOffice/Configuration/ViewModels/IOBackOfficeConfigurationControllerDefaultViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using IOBootstrap.NET.DataAccess.Context;
-
-namespace IOBootstrap.NET.BackOffice.Configuration.ViewModels
-{
-    public class IOBackOfficeConfigurationControllerDefaultViewModel : IOBackOfficeConfigurationsViewModel<IODatabaseContextDefaultImpl>
-    {
-    }
-}

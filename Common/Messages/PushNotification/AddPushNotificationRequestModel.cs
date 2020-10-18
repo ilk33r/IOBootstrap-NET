0b2b294227ec7c73bcce3c6f253908156889a915
Ilker OZCAN <ilker.ozcan@veripark.com>
2020-10-18 19:01:00 +0300

    Push notification request and response models were migrated with netcore 3.1

---
+++ Common/Messages/PushNotification/AddPushNotificationRequestModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using IOBootstrap.NET.Common.Enumerations;
+using IOBootstrap.NET.Common.Messages.Base;
+
+namespace IOBootstrap.NET.Common.Messages.PushNotification
+{
+    public class AddPushNotificationRequestModel : IORequestModel
+    {
+		[Required]
+		public int AppBuildNumber { get; set; }
+
+		[Required]
+		[StringLength(64)]
+		public String AppBundleId { get; set; }
+
+		[Required]
+		[StringLength(10)]
+		public String AppVersion { get; set; }
+
+		[Required]
+		[StringLength(128)]
+		public String DeviceId { get; set; }
+
+		[Required]
+		[StringLength(128)]
+		public String DeviceName { get; set; }
+
+		[Required]
+		[StringLength(256)]
+		public String DeviceToken { get; set; }
+
+		[Required]
+        public DeviceTypes DeviceType { get; set; }
+
+    }
+}
---
+++ Common/Messages/PushNotification/AddPushNotificationResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IOBootstrap.NET.Common.Messages.Base;
+
+namespace IOBootstrap.NET.Common.Messages.PushNotification
+{
+    
+    public class AddPushNotificationResponseModel : IOResponseModel
+    {
+        public AddPushNotificationResponseModel(int responseStatusMessage) : base(responseStatusMessage)
+        {
+        }
+    }
+}

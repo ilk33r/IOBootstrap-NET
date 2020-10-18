c80330a49aa975c355cec7f9b7bc48dae81786ba
Ilker OZCAN <ilker.ozcan@veripark.com>
2020-10-18 18:45:19 +0300

    Solution was updated.

--- SupportingFiles/migratedatabase.sh
+++ SupportingFiles/migratedatabase.sh
@@ -5,3 +5,7 @@ cd ./${PROJECT_DIR}
 dotnet ef database drop --verbose
 dotnet ef migrations add InitialCreate --verbose
 dotnet ef database update
+
+
+dotnet ef migrations add X --context IODefaultDatabaseContext --verbose
+dotnet ef migrations script Y --context IODefaultDatabaseContext --verbose -o ../Y.sql

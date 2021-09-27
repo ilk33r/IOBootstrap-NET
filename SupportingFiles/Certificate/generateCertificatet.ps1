703c9a76e8b682db8b55653158615c417d99b879
Ilker OZCAN <ilker.ozcan@veripark.com>
2021-09-27 20:15:19 +0300

    SSL certificate generate script was updated.

--- SupportingFiles/Certificate/generateCertificatet.ps1
+++ SupportingFiles/Certificate/generateCertificatet.ps1
@@ -1,5 +1,9 @@
+# https://github.com/rmbolger/Posh-ACME/blob/main/docs/Tutorial.md
+
 Set-PAServer LE_PROD
 
-New-PACertificate 'api.comigofit.com','backoffice.comigofit.com','jobs.comigofit.com','app.comigofit.com','www.comigofit.com','comigofit.com' -AcceptTOS -Contact 'iletisim@ilkerozcan.com.tr'
+New-PACertificate 'api.hopdedi.com','backoffice.hopdedi.com','app.hopdedi.com','www.hopdedi.com','hopdedi.com' -AcceptTOS -Contact 'iletisim@ilkerozcan.com.tr'
 
 Get-PACertificate | fl
+
+# The password on the PFX files is poshacme 

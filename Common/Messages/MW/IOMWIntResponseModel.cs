1e88571761b1e37434b9eb5b80f2e11f0d0207c4
Ilker OZCAN <iletisim@ilkerozcan.com.tr>
2022-03-12 13:53:58 +0300

    MW Int response model was added.

---
+++ Common/Messages/MW/IOMWIntResponseModel.cs
@@ -0,0 +1,32 @@
+using System;
+using IOBootstrap.NET.Common.Messages.Base;
+
+namespace IOBootstrap.Net.Common.Messages.MW
+{
+    public class IOMWIntResponseModel : IOResponseModel
+    {
+        #region Properties
+
+		public int Item { get; set; }
+
+		#endregion
+
+        #region Initialization Methods
+
+		public IOMWIntResponseModel() : base() 
+		{
+		}
+
+		public IOMWIntResponseModel(int responseStatusMessage) : base(responseStatusMessage) 
+		{
+		}
+
+        public IOMWIntResponseModel(int responseStatusMessage, int item) : base(responseStatusMessage)
+		{
+            Item = item;
+		}
+
+		#endregion
+    }
+
+}
